b4a90de2c712a9c62367eadd016ca192e927d9d2
AC <albert1chan@foxmail.com>
2024-01-26 16:21:21 +0800

    fix: support web3.py V6

--- zksync_sdk/__init__.py
+++ zksync_sdk/__init__.py
@@ -1,3 +1,7 @@
+import web3
+if web3.__version__ >= '6.0.0':
+    web3.Web3.toChecksumAddress = web3.Web3.to_checksum_address
+
 from .ethereum_provider import EthereumProvider
 from .ethereum_signer import EthereumSignerWeb3
 from .lib import ZkSyncLibrary
